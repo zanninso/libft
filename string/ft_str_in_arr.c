98d386610456213919f0b91c7beb79d1110ff090
Amine Ait ihia <aait-ihi@e2r4p10.1337.ma>
2019-11-26 00:40:30 +0100

    hjhadj

--- string/ft_str_in_arr.c
+++ string/ft_str_in_arr.c
@@ -3,23 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_in_arr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aait-ihi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aait-ihi <aait-ihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 03:44:05 by aait-ihi          #+#    #+#             */
-/*   Updated: 2019/11/25 03:44:17 by aait-ihi         ###   ########.fr       */
+/*   Updated: 2019/11/26 00:30:53 by aait-ihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-_Bool   ft_str_in_arr(char *str, char **tab)
+_Bool	ft_str_in_arr(char *str, char **tab)
 {
-    if (!str)
-        while (*tab)
-        {
-            if(ft_strequ(str, *tab))
-                return(1);
-            tab++;
-        }
-    return (0);
+	if (str && tab)
+	{
+		while (*tab)
+		{
+			if (ft_strequ(str, *tab))
+				return (1);
+			tab++;
+		}
+	}
+	return (0);
 }
