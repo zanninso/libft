f5ac8d8f8c5cc02b9aacc4580a63089bf54340cd
Amine Ait ihia <aait-ihi@e1r8p10.1337.ma>
2020-11-02 20:50:35 +0100

    end dtd parse; start xml validation by against dtd

--- xml/dtd_validator.c
+++ xml/dtd_validator.c
@@ -6,17 +6,19 @@
 /*   By: aait-ihi <aait-ihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 13:56:16 by aait-ihi          #+#    #+#             */
-/*   Updated: 2020/11/02 20:46:01 by aait-ihi         ###   ########.fr       */
+/*   Updated: 2020/11/02 20:49:46 by aait-ihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "xml.h"
 
-bool dtd_validate(t_xm)
+bool dtd_validate(t_xml_tag *tag, t_dtd dtd)
 {
     while ()
     {
-        /* code */
+        if()
+       dtd_validate()
     }
     
 }
